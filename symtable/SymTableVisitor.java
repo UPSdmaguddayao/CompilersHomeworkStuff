2703a521c4ad16ff5342936fea2c42fb36d16a92
dmaguddayao <djmag93@djmagudayaosmbp.pugetsound.edu>
2015-03-13 16:57:03 -0700

    FUCKED UP

--- symtable/SymTableVisitor.java
+++ symtable/SymTableVisitor.java
@@ -1,13 +1,13 @@
 package symtable;
 
 import minijava.analysis.DepthFirstAdapter;
-import minijava.node.*;
 import minijava.node.ABaseClassDecl;
 import minijava.node.AMainClassDecl;
 import minijava.node.ASubClassDecl;
 
 import java.io.PrintWriter;
 import java.util.*;
+import java.io.File;
 
 /** 
  * This visitor class builds a symbol table as it traverses the tree.  The
@@ -42,22 +42,19 @@ public class SymTableVisitor extends DepthFirstAdapter
    public void caseAMainClassDecl(AMainClassDecl node)
    {
       inAMainClassDecl(node);
-      indent();
-      out.print("class ");
       depth++;
       if(node.getId() != null)
-      {
-         out.print(node.getId().getText());
+      {  
+       /*  try
+         {
+         table.put(node.getId(),null,node.getVarDecl(), node.getMethod());
+         }
+         catch (ClassClashException e)
+         {
+            System.err.println("FUCKED UP");
+         }*/
       }
-      out.println(" {");
-      indent(); out.println("public static void main(String[] bogus) {");
-      if(node.getStmt() != null)
-      {
-         node.getStmt().apply(this);
-      }
-      indent(); out.println("}");
       depth--;
-      indent(); out.println("}\n");
       outAMainClassDecl(node);
    }
    
@@ -66,27 +63,18 @@ public class SymTableVisitor extends DepthFirstAdapter
    {
       inABaseClassDecl(node);
       
-      indent(); out.print("class ");
+      //indent(); out.print("class ");
       if(node.getId() != null)
       {
-         out.print(node.getId().getText());
-      }
-      out.println(" {");
-      {
-         List<PVarDecl> copy = new ArrayList<PVarDecl>(node.getVarDecl());
-         for(PVarDecl e : copy)
+         try
          {
-            e.apply(this);
+            table.put(node.getId(), null, node.getVarDecl(), node.getMethod());
+         }
+         catch (Exception e)
+         {
+            System.err.println("FUCKED UP");
          }
       }
-      {
-         List<PMethod> copy = new ArrayList<PMethod>(node.getMethod());
-         for(PMethod e : copy)
-         {
-            e.apply(this);
-         }
-      }
-      indent(); out.println("}\n");
       outABaseClassDecl(node);
    }
    
@@ -94,96 +82,18 @@ public class SymTableVisitor extends DepthFirstAdapter
    public void caseASubClassDecl(ASubClassDecl node)
    {
       inASubClassDecl(node);
-      out.print("class ");
       if(node.getId() != null)
       {
-         out.print(node.getId().getText());
-      }
-      out.print(" extends ");
-      if(node.getExtends() != null)
-      {
-         node.getExtends().apply(this);
-         out.print(node.getExtends().getText());
-      }
-      out.println(" {");
-      {
-         List<PVarDecl> copy = new ArrayList<PVarDecl>(node.getVarDecl());
-         for(PVarDecl e : copy)
+      try{
+            table.put(node.getId(),node.getExtends(), node.getVarDecl(), node.getMethod());
+         }
+         catch (Exception e)
          {
-            e.apply(this);
+            System.err.println("FUCKED UP");
          }
       }
-      {
-         List<PMethod> copy = new ArrayList<PMethod>(node.getMethod());
-         for(PMethod e : copy)
-         {
-            e.apply(this);
-         }
-      }
-      out.println("}\n");
       outASubClassDecl(node);
    }
    
    
-   public void caseAVarDecl(AVarDecl node)
-   {
-      inAVarDecl(node);
-      indent();
-      if(node.getType() != null)
-      {
-         node.getType().apply(this);
-      }
-      out.print(" ");
-      if(node.getId() != null)
-      {
-         out.print(node.getId().getText());
-      }
-      out.println(";");
-      outAVarDecl(node);
-   }
-   
-   
-   public void caseAMethod(AMethod node)
-   {
-      inAMethod(node);
-      out.println();
-      indent(); out.print("public ");
-      if(node.getType() != null)
-      {
-         node.getType().apply(this);
-      }
-      out.print(" ");
-      if(node.getId() != null)
-      {
-         out.print(node.getId().getText());
-      }
-      out.print("(");
-      {
-         List<PFormal> copy = new ArrayList<PFormal>(node.getFormal());
-         int len = copy.size();
-         for(int i=0; i<len-1; i++) {
-            copy.get(i).apply(this);
-            out.print(", ");
-         }
-         if (len > 0)
-            copy.get(len-1).apply(this);
-      }
-      out.println(") {");
-      {
-         List<PVarDecl> copy = new ArrayList<PVarDecl>(node.getVarDecl());
-         for(PVarDecl e : copy)
-         {
-            e.apply(this);
-         }
-      }
-      {
-         List<PStmt> copy = new ArrayList<PStmt>(node.getStmt());
-         for(PStmt e : copy)
-         {
-            e.apply(this);
-         }
-      }
-      indent(); out.println("}");
-      outAMethod(node);
-   }
 }
